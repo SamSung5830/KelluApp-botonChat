12dd6940d41353583d1c1a2c50bd52c9090f7570
Usuario-THINK\Usuario <ssung5830@gmail.com>
2018-02-02 00:08:20 -0300

    para crear apk 02022018

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,19 +1,8 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-  {
-    "id": "cordova-plugin-inappbrowser.inappbrowser",
-    "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
-    "pluginId": "cordova-plugin-inappbrowser",
-    "clobbers": [
-      "cordova.InAppBrowser.open",
-      "window.open"
-    ]
-  }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-inappbrowser": "2.0.1",
   "cordova-plugin-whitelist": "1.3.3"
 };
 // BOTTOM OF METADATA
